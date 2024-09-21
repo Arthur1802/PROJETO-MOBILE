70c9364553bb604ea3e45b96b5bd7fc92fe432e8
Arthur Quinellato <103618245+Arthur1802@users.noreply.github.com>
2024-09-20 21:33:11 -0300

    Estilizar página de login

--- am2vlearn/src/App.js
+++ am2vlearn/src/App.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react'
 import './styles/App.css'
-import lg_logo from './assets/lg_logo_light'
 import Login from './pages/LogIn'
-import Home from './pages/Home'
 import Welcome from './pages/Welcome'
 
 const App = () => {
@@ -10,7 +8,6 @@ const App = () => {
 
   return (
     <div>
-      <img src = {lg_logo} alt = "Logo"></img>
       {/* {isLogged ? <Home/> : <Welcome setIsLogged = {setIsLogged, theme}/>} */}
       <Login />
     </div>
--- am2vlearn/src/pages/LogIn.jsx
+++ am2vlearn/src/pages/LogIn.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
-import sm_logo from '../assets/sm_logo_light'
-import google_icon from '../assets/google_icon'
+import '../styles/Login.css'
+import sm_logo from '../assets/sm_logo_light.svg'
+import google_icon from '../assets/google_icon.svg'
 
 const LogIn = () => {
     const [values, setValues] = useState({
@@ -10,42 +11,54 @@ const LogIn = () => {
 
     const [mostrarSenha, setMostrarSenha] = useState(false)
 
+    const handleInput = (e) => {
+        setValues(prev => ({ ...prev, [e.target.name]: e.target.value }));
+    }
+
     return (
         <div>
-            <a><i className = "fa-solid fa-arrow-left"></i></a> {/* Botão para voltar para página inicial */}
-            <img src = {sm_logo} alt = "Logo"></img>
-            <label for = "cred_inp">CONTA:</label>
-            <input
-                type = "text"
-                name = "credencial"
-                id = "cred_inp"
-                placeholder = "Insira aqui seu e-mail ou seu nome"
-                onChange = {handleInput}
-            />
-            <div className = "senha_cont">
-                <label for = "senha_inp">SENHA:</label>
-                <input
-                    type = {mostrarSenha ? "text" : "password"}
-                    name = "senha"
-                    id = "senha_inp"
-                    placeholder = "Insira aqui sua senha"
-                    onChange = {handleInput}
-                />
-                <i className = {mostrarSenha ? "fa-solid fa-eye-slash" : "fa-solid fa-eye"} onClick = {() => setMostrarSenha(!mostrarSenha)}></i>
-            </div>
-            <button id = "btnLogin">ENTRAR</button>
+            <div className = "login-panel">
+                <div className = "arrow-cont">
+                    <a className = "backBtn"><i className = "fa-solid fa-arrow-left"></i></a> {/* Botão para voltar para página inicial */}
+                </div>
+                <div className = "img-cont">
+                    <img className = "login-logo" src = {sm_logo} alt = "Logo"></img>
+                </div>
+                <div className = "form">
+                    <label for = "cred_inp">CONTA:</label>
+                    <input
+                        type = "text"
+                        name = "credencial"
+                        id = "cred_inp"
+                        placeholder = "Insira aqui seu e-mail ou seu nome"
+                        onChange = {handleInput}
+                    />
+                    <div className = "senha_cont">
+                        <label for = "senha_inp">SENHA:</label>
+                        <input
+                            type = {mostrarSenha ? "text" : "password"}
+                            name = "senha"
+                            id = "senha_inp"
+                            placeholder = "Insira aqui sua senha"
+                            onChange = {handleInput}
+                        />
+                        <i className = {mostrarSenha ? "fa-solid fa-eye" : "fa-solid fa-eye-slash"} onClick = {() => setMostrarSenha(!mostrarSenha)}></i>
+                    </div>
+                    <button id = "btnLogin">ENTRAR</button>
 
-            <div className = "separador"> {/* -------------- OU -------------- */}
-                <span></span>
-                <p>OU</p>
-                <span></span>
+                    <div className = "separador"> {/* -------------- OU -------------- */}
+                        <span></span>
+                        <p>OU</p>
+                        <span></span>
+                    </div>
+                    
+                    <button className = "btnGoogle">
+                        <img src = {google_icon} alt = ""></img>
+                        GOOGLE
+                    </button>
+                    <button className = "btnCriarConta">CRIAR CONTA</button>
+                </div>
             </div>
-            
-            <button className = "btnGoogle">
-                <img src = {google_icon} alt = ""></img>
-                GOOGLE
-            </button>
-            <button className = "btnCriarConta">CRIAR CONTA</button>
         </div>
     )
 }
