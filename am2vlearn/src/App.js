59f0f5989de20e4387e8a463812ce608f1f6d012
Arthur Quinellato <arthur.quinellato@gmail.com>
2024-09-23 16:07:49 -0300

    Adicionar o botao de mudança de tema

--- am2vlearn/src/App.js
+++ am2vlearn/src/App.js
@@ -1,22 +1,23 @@
 import React from 'react' // Adicionar o { useState }
 import './styles/App.css'
 import Welcome from './pages/Welcome'
+import useLocalStorage from 'use-local-storage-state'
+import AlternaTema from './components/AlternaTema'
 
 const App = () => {
   // const [isLogged, setIsLogged] = useState(false)
 
-  // const preferencia = window.matchMedia('(prefers-color-scheme: dark)').matches // Adicionar o tema preferido
-  // const [tema, setTema] = useLocalStorage('tema', preferencia) // Adicionar o useLocalStorage (npm install use-local-storage)
+  const preferencia = window.matchMedia('(prefers-color-scheme: dark)').matches // Adicionar o tema preferido
+  const [tema, setTema] = useLocalStorage('tema', preferencia) // Adicionar o useLocalStorage (npm install use-local-storage)
 
   
 
   return (
-    // <div data-theme = {tema ? 'dark' : 'light'}>
-    <div>
-      {/* <AlternaTema
+    <div data-theme = {tema ? 'dark' : 'light'}>
+      <AlternaTema
         isChecked = {tema}
         hendleChange = {() => setTema(!tema)}
-      /> */}
+      />
       {/* {isLogged ? <Home/> : <Welcome setIsLogged = {setIsLogged, theme}/>} */}
       <Welcome />
     </div>
