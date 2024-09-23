4a1d7345356526c7427843cb37427b69f613ffa4
Arthur Quinellato <arthur.quinellato@gmail.com>
2024-09-22 22:01:36 -0300

    ...

--- am2vlearn/src/App.js
+++ am2vlearn/src/App.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React from 'react' // Adicionar o { useState }
 import './styles/App.css'
 import Welcome from './pages/Welcome'
 
 const App = () => {
   // const [isLogged, setIsLogged] = useState(false)
 
+  // const preferencia = window.matchMedia('(prefers-color-scheme: dark)').matches // Adicionar o tema preferido
+  // const [tema, setTema] = useLocalStorage('tema', preferencia) // Adicionar o useLocalStorage (npm install use-local-storage)
+
+  
+
   return (
+    // <div data-theme = {tema ? 'dark' : 'light'}>
     <div>
+      {/* <AlternaTema
+        isChecked = {tema}
+        hendleChange = {() => setTema(!tema)}
+      /> */}
       {/* {isLogged ? <Home/> : <Welcome setIsLogged = {setIsLogged, theme}/>} */}
       <Welcome />
     </div>
