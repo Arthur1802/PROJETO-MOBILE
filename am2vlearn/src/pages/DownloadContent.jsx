ab69e912709c68da709aa6f86a8e188c890427e1
Matheus Petrakis <86805775+Petrakis20@users.noreply.github.com>
2024-10-02 13:56:43 -0300

    Download Content page - feita

--- am2vlearn/src/pages/DownloadContent.jsx
+++ am2vlearn/src/pages/DownloadContent.jsx
@@ -1,40 +1,50 @@
 import React from 'react'
 
-const DownloadContent = ({ option }) => {
-    let imgSrc
-    let tema = 'dark'
+import download_icon from '../assets/icons/download_icon.svg'
+import play_icon from '../assets/icons/play_icon.svg'
+import imgSrc from '../assets/icons/html_dark.svg'
 
-    switch (option) {
-        case 'html': 
-            imgSrc = require(`../assets/icons/html-${tema}.png`)
-            break
-        case 'css':
-            imgSrc = require(`../assets/icons/css-${tema}.png`)
-            break
-        case 'js':
-            imgSrc = require(`../assets/icons/js-${tema}.png`)
-            break
-        case 'all':
-            imgSrc = require(`../assets/icons/grouped_logos_${tema}.png`)
-            break
-    }
+import '../styles/pages/DownloadContent.css'
+
+const DownloadContent = ({ option }) => {
+    // let imgSrc
+    // let tema = 'dark'
+
+    // switch (option) {
+    //     case 'html': 
+    //         imgSrc = require(`../assets/icons/html-${tema}.png`)
+    //         break
+    //     case 'css':
+    //         imgSrc = require(`../assets/icons/css-${tema}.png`)
+    //         break
+    //     case 'js':
+    //         imgSrc = require(`../assets/icons/js-${tema}.png`)
+    //         break
+    //     case 'all':
+    //         imgSrc = require(`../assets/icons/grouped_logos_${tema}.png`)
+    //         break
+    // }
 
     return (
-        <div>
-            <img
-                src = {imgSrc}
-                alt = "Content"
-            />
-            <button>
-                BAIXAR MATERIAL DE APOIO
-                <img src = {download_icon} alt = "" />
-            </button>
-            <button>
+        <div className='download-content-panel'>
+            <div className="content">
                 <img
-                    src = {download_icon}
-                    alt = ""
+                    className='content-logo'
+                    src = {imgSrc}
+                    alt = "Content"
                 />
-            </button>
+                <button className='download-content-button poppins-semibold'>
+                    BAIXAR MATERIAL DE APOIO
+                    <img src = {download_icon} alt = "" />
+                </button>
+                <button className='start-button poppins-semibold'>
+                    COMEÇAR
+                    <img
+                        src = {play_icon}
+                        alt = ""
+                    />
+                </button>
+            </div>
         </div>
     )
 }
