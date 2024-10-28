100eac82aaffb0b4c70a7711506ac0988349356d
Matheus Petrakis <86805775+Petrakis20@users.noreply.github.com>
2024-10-28 12:53:28 -0300

    mobile css

--- am2vlearn/src/pages/Welcome/Welcome.jsx
+++ am2vlearn/src/pages/Welcome/Welcome.jsx
@@ -14,8 +14,8 @@ const Welcome = () => {
                     <img className = "welcome-logo" src = {lg_logo} alt = "Logo"></img>
                 </div>
                 <div className = "btn-cont">
-                    <Link className = "btnsWelcome azul-claro" id = "btnLogin" to = "/login">ENTRAR</Link>
-                    <Link className = "btnsWelcome laranja" id = "btnCriarConta" to = "/signin">CRIAR CONTA</Link>
+                    <Link className = "btnsWelcome azul-claro poppins-semibold" id = "btnLogin" to = "/login">ENTRAR</Link>
+                    <Link className = "btnsWelcome laranja poppins-semibold" id = "btnCriarConta" to = "/signin">CRIAR CONTA</Link>
                 </div>
             </div>
         </div>
