bea5f3caa12ee68d18e085226ec2c8c467e83ddc
Arthur1802 <arthur.barreto@soulasalle.com.br>
2024-10-29 20:16:36 -0300

    ...

--- am2vlearn/src/utils/authentication/auth.js
+++ am2vlearn/src/utils/authentication/auth.js
@@ -16,36 +16,36 @@ if (localStorage.getItem('userLoggedIn') === null) {
 
 export const Login = async (email, senha) => {
     if (!emailRegex.test(email)) {
-        return {errors: {email: "Email inválido"}}
+        return { errors: { email: "Email inválido" } }
     }
 
     if (senha.length < 8) {
-        return {errors: {senha: "Senha deve ter no mínimo 8 caracteres"}}
+        return { errors: { senha: "Senha deve ter no mínimo 8 caracteres" } }
     }
 
     try {
         const credencial = await signInWithEmailAndPassword(auth, email, senha)
         const user = credencial.user
 
-        
+
         const dbRefUsuario = ref(db, `usuarios/${user.uid}`)
         const dataSnapshot = await get(query(dbRefUsuario))
-        
+
         if (dataSnapshot.exists()) {
             localStorage.setItem('userLoggedIn', true)
             localStorage.setItem('userUID', user.uid)
-            
+
             if (!user.emailVerified) {
-                return {warning: {login: "Email não verificado. Verifique sua caixa de entrada."}}
+                return { warning: { login: "Email não verificado. Verifique sua caixa de entrada." } }
             }
 
-            return {success: {login: "Login efetuado com sucesso!"}}
+            return { success: { login: "Login efetuado com sucesso!" } }
         } else {
-            return {errors: {login: "Usuário não registrado. Crie uma conta e tente novamente."}}
+            return { errors: { login: "Usuário não registrado. Crie uma conta e tente novamente." } }
         }
     } catch (erro) {
         console.error(erro)
-        return {errors: {login: `Erro ao fazer login\n${erro.message}`}}
+        return { errors: { login: `Erro ao fazer login\n${erro.message}` } }
     }
 }
 
@@ -60,37 +60,37 @@ export const LoginWithGoogle = async () => {
             localStorage.setItem('userUID', user.uid)
 
             if (!user.emailVerified) {
-                return {warning: {loginWithGoogle: "Email não verificado. Verifique sua caixa de entrada."}}
+                return { warning: { loginWithGoogle: "Email não verificado. Verifique sua caixa de entrada." } }
             }
 
-            return {success: {loginWithGoogle: "Login efetuado com Google com sucesso."}}
+            return { success: { loginWithGoogle: "Login efetuado com Google com sucesso." } }
         } else {
-            return {errors: {loginWithGoogle: "Usuário não registrado. Crie uma conta e tente novamente."}}
+            return { errors: { loginWithGoogle: "Usuário não registrado. Crie uma conta e tente novamente." } }
         }
     } catch (erro) {
         console.error(erro)
-        return {errors: {loginWithGoogle: `Erro ao fazer login com Google\n${erro.message}`}}
+        return { errors: { loginWithGoogle: `Erro ao fazer login com Google\n${erro.message}` } }
     }
 }
 
 export const Signin = async (nome, email, senha) => {
     if (!nome) {
-        return {errors: {nome: "Nome é obrigatório"}}
+        return { errors: { nome: "Nome é obrigatório" } }
     }
 
     if (!emailRegex.test(email)) {
-        return {errors: {email: "Email inválido"}}
+        return { errors: { email: "Email inválido" } }
     }
 
     if (senha.length < 8) {
-        return {errors: {senha: "Senha deve ter no mínimo 8 caracteres"}}
+        return { errors: { senha: "Senha deve ter no mínimo 8 caracteres" } }
     }
 
     try {
         const credencial = await createUserWithEmailAndPassword(auth, email, senha)
         const user = credencial.user
 
-        
+
         const dbRefUsuario = ref(db, `usuarios/${user.uid}`)
         const objUsuario = {
             uid: user.uid,
@@ -98,15 +98,15 @@ export const Signin = async (nome, email, senha) => {
             email: user.email,
             funcao: "ALUNO"
         }
-        
+
         await set(dbRefUsuario, objUsuario)
         await sendEmailVerification(user)
-        
+
         console.log("Conta criada com sucesso. Email de verificação enviado")
-        return {success: {signin: "Conta criada com sucesso. Email de verificação enviado."}}
+        return { success: { signin: "Conta criada com sucesso. Email de verificação enviado." } }
     } catch (erro) {
         console.error(erro)
-        return {errors: {signin: `Erro ao criar conta\n${erro.message}`}}
+        return { errors: { signin: `Erro ao criar conta\n${erro.message}` } }
     }
 }
 
@@ -154,4 +154,13 @@ export const LogOut = async () => {
 
 export const isLoggedIn = () => {
     return JSON.parse(localStorage.getItem('userLoggedIn') || 'false')
+}
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js', { scope: '/' })
+        .then(function (reg) {
+            console.log('Registro do Service Worker bem sucedido. O escopo de uso é ' + reg.scope)
+        }).catch(function (error) {
+            console.log('Registro do Service Worker com ' + error)
+        })
 }
