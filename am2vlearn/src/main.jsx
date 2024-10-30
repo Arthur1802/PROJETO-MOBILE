e34850e522e9610278326e4dc416d49b8f8bb233
Arthur1802 <arthur.barreto@soulasalle.com.br>
2024-10-30 12:59:28 -0300

    ....

--- am2vlearn/src/main.jsx
+++ am2vlearn/src/main.jsx
@@ -2,12 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-import { CounterHtmlContextProvider } from './context/CounterHtmlContext.jsx'
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <CounterHtmlContextProvider>
-        <App /> 
-    </CounterHtmlContextProvider>
+    <App />
   </StrictMode>,
 )
